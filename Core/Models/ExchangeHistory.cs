89f555cfdd281c34231174ae198edc20ea6d1a37
Muhammad Sherif <muhammadsherif.wo@gmail.com>
2022-04-21 01:08:47 +0200

    Add Core Models

---
+++ Core/Models/ExchangeHistory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+	public class ExchangeHistory
+	{
+		public int Id { get; set; }
+		public int CurrencyId{ get; set; }
+		public DateTime ExchangeDate{ get; set; }	
+		public double Rate{ get; set; }
+
+		public Currency Currency { get; set; }
+
+	}
+}
