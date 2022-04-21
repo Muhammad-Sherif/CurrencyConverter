af7209cae7da61ca76861fd151ebe4de0c509df2
Muhammad Sherif <muhammadsherif.wo@gmail.com>
2022-04-21 19:00:00 +0200

    Add Automapper

---
+++ Web/AutoMapperProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Core.Models;
+using Web.Dtos;
+
+namespace Web
+{
+	public class AutoMapperProfile : Profile
+	{
+		public AutoMapperProfile()
+		{
+			CreateMap<Currency, CurrencyDto>();
+			CreateMap<CreateCurrencyDto, Currency>();
+			CreateMap<UpdateCurrencyDto, Currency>();
+
+		}
+	}
+}
