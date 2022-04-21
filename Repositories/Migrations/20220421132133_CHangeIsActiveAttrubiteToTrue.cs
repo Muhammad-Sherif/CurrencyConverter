5e6ccfbaff4a6fa18b671f47cfc333f62bd3a216
Muhammad Sherif <muhammadsherif.wo@gmail.com>
2022-04-21 18:58:25 +0200

    Add IsActive Attrubites changes migrations

---
+++ Repositories/Migrations/20220421132133_CHangeIsActiveAttrubiteToTrue.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repositories.Migrations
+{
+    public partial class CHangeIsActiveAttrubiteToTrue : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Currencies",
+                type: "bit",
+                nullable: false,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldDefaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Currencies",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldDefaultValue: true);
+        }
+    }
+}
